e132de549d98437537bf1679c8ac9b7caff23a6e
setlol <dovnar.maxim@gmail.com>
2016-10-16 14:44:15 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+char* deleteCharInArray(char* str, int numberStr);
+int main()
+{
+	int count = 28;
+	char* str = new char[28]{"abcdifghijklmnopqrstuvwxyz\0"};
+	int exit;
+	int numberStr;
+	do
+	{
+		cout << "str: " << str << endl;
+		int chek = 0;
+		do
+		{
+			cout << "enter number delete: ";
+			cin >> numberStr;
+			if (numberStr >= 0 && numberStr < count)
+			{
+				chek++;
+			}
+			else
+			{
+				cout << "don't number for delete!!!" << endl;
+			}
+		} while (chek == 0);
+		cout << "new str: " << deleteCharInArray(str, numberStr)<< endl << "restart: 1\nexit: 0\nenter";
+		cin >> exit;
+		count--;
+	} while (exit != 0);
+	delete[] str;
+}
+
+char * deleteCharInArray(char * str, int numberStr)
+{
+	strcpy(&str[numberStr], &str[numberStr + 1]);
+	return str;
+}
+
+//Написать функцию, которая удаляет из строки символ с заданным номером.
---
+++ task2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+char* deleteCharInArray(char* str, char* simbol);
+int main()
+{
+	char* str = new char[28]{ "abcdifghijklmnopqrstuvwxyz\0" };	int exit;
+	char* simbol = new char[2];
+	int h = 0;
+	do
+	{
+		cout << "str: " << str << endl << "enter simbol delete: ";
+		if (h != 0)										//если оставить без проверки на первый вход то не отрабатывает удаление 
+		{
+			getchar();
+		}
+		h++;
+		cin.getline(simbol, 2);
+		str = deleteCharInArray(str, simbol);
+		cout << "new str: " << str << endl << "restart: 1\nexit: 0\nenter";
+		cin >> exit;
+	} while (exit != 0);
+	delete[] str;
+	delete[] simbol;
+}
+
+char* deleteCharInArray(char* str, char* simbol)
+{
+	int check = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == *simbol)
+		{
+			strcpy(&str[i], &str[i + 1]);
+			check++; 
+		}
+	}
+	if (check < 1)
+	{
+		cout << "No changes";
+	}
+	return str;
+}
+
+//Написать функцию, которая удаляет из строки все вхождения в нее заданного символа.
